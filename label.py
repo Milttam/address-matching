a9dcb3067e85a3f36b837cdec2c98f929f07175b
Matt Lim <sl2533@cornell.edu>
2023-11-12 16:25:03 -0500

    Added geocoding function and label function: - Implemented full functionality to geocoding using free API - Create label.py file

---
+++ label.py
@@ -0,0 +1,9 @@
+def label_address(address):
+    """
+    Given un unstructured address, return an address dictionary with corresponding labels
+    Ideally, use address labeling API using Conditional Random Fields to label the address, 
+    such as the libpostal API
+    """
+    # Currently, I am having trouble with installing libpostal and getting
+    # the dependencies/datasets to connect
+    pass
