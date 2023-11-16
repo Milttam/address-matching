1f03488c65765feaaae1e47e1a266a329c12410a
Matt Lim <sl2533@cornell.edu>
2023-11-16 14:53:50 -0500

    Completed extra functions - ithaca_addresses.json are the top 200 providers in Ithaca found by NPPES API - process_json.py processes the json file to extract the list of practice locations - generate_pair.py generates a scrambled/similar address given a "correct" address object - label.py is yet to be implemented due to problems with libpostal/parsing libraries

--- label.py
+++ label.py
@@ -2,7 +2,7 @@ def label_address(address):
     """
     Given un unstructured address, return an address dictionary with corresponding labels
     Ideally, use address labeling API using Conditional Random Fields to label the address, 
-    such as the libpostal API
+    such as the libpostal API/uspostal python library
     """
     # Currently, I am having trouble with installing libpostal and getting
     # the dependencies/datasets to connect
