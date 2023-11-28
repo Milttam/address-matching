eea408a785b9d03e6929492928cfb5bcd8e520aa
Matt Lim <sl2533@cornell.edu>
2023-11-28 14:30:50 -0500

    Added geocoding and processing json file

--- process_json.py
+++ process_json.py
@@ -17,7 +17,8 @@ def process_json(json_data):
                     "street_address": address["address_1"],
                     "city": address["city"],
                     "state": address["state"],
-                    "zip": address["postal_code"],
+                    # only take first 5 digits of postal code
+                    "zip": address["postal_code"][:5]
                 }
                 location_addresses.append(address)
 
@@ -28,4 +29,4 @@ with open('data/ithaca_addresses.json') as json_file:
     data = json.load(json_file)
 
 location_addresses = process_json(data)
-print(location_addresses[:5])
+print(location_addresses)
