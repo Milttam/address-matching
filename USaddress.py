ef48319c0da51d0d179300e181df9033f6f54228
Elisa Kelly <136484462+elisackelly@users.noreply.github.com>
2023-12-01 12:55:41 -0500

    Update USaddress.py
    Commenting on structure of the file

--- USaddress.py
+++ USaddress.py
@@ -3,7 +3,7 @@ import usaddress
 address_a='136 Hoy Rd, Ithaca, NY 14850'
 address_b ='401 Cayuga Park Ln Suite 101, Ithaca, NY 14850'
 address_c='101 Dates Dr, Ithaca, NY 14850'
-# address_c=
+# address dictionary for parsing.
 address_map_dict={
    'AddressNumber': 'Street Number',
    'AddressNumberPrefix': 'Street Number',
@@ -24,5 +24,6 @@ address_map_dict={
    'PlaceName': 'City',
    'StateName': 'State',
    'ZipCode': 'Postal_code',}
+#examples using the dictionary
 example = usaddress.tag(address_c, tag_mapping=address_map_dict)
 example2 = usaddress.tag('Cayuga Medical Center '+address_c, tag_mapping=address_map_dict)
